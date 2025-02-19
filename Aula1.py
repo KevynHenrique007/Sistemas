3a340f59ab23fea944f5d0a4b5e659cdfbfab897
Kevyn <kevyn.silvano@escola.pr.gov.br>
2025-02-19 12:19:29 -0300

    Adicionado

--- Aula1.py
+++ Aula1.py
@@ -11,3 +11,16 @@ if (chute == sorteio):
     print("Parabéns, você acertou o número! o número era ", sorteio)
 if (chute !=sorteio):
     print("Você errou! O número era", sorteio)
+
+limite_tentativas = 3
+tentativa = 1
+while (limite_tentativas >= tentativa):
+    print("Tentativa", tentativa)
+    chute = int(input("Digite o seu chute:"))
+    if (chute == sorteio):
+        print("Parabéns, você acertou!")
+    elif (chute > sorteio):
+        print("Chute um número menor!")
+    elif (chute < sorteio):
+        print("Chute um número menor!")
+    tentativa = tentativa + 1 
