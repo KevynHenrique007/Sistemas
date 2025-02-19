88c8bda005c71b5e17c86d3b02408f6fe84bf390
Kevyn <kevyn.silvano@escola.pr.gov.br>
2025-02-19 12:10:58 -0300

    Adicionado

--- Aula1.py
+++ Aula1.py
@@ -1,6 +1,13 @@
 import random
 
 sorteio = random.randint(1,10)
-chute = input("digite seu chute:")
+print("###  JOGO DA ADIVINHAÇÃO")
+print("Adivinhe o número que estou pensando, de 1 a 10")
+chute = int(input("digite seu chute:"))
 print(sorteio)
 print(chute)
+
+if (chute == sorteio):
+    print("Parabéns, você acertou o número! o número era ", sorteio)
+if (chute !=sorteio):
+    print("Você errou! O número era", sorteio)
